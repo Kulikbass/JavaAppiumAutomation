5fb16452bb476e470a4228e7724f1076e63b6f54
vladislavkulakov <kulikbass@gmail.com>
2024-01-15 22:33:49 +0300

    Third Test

---
+++ src/ThirdTest/MainClass.java
@@ -0,0 +1,13 @@
+package ThirdTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClass {
+
+    private String class_string = "Hello, world";
+
+    public String getClassString(){
+        return class_string;
+    }
+}
