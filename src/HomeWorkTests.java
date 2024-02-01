eb904e56c53a6ae38f6a1c29c9e3487e14706596
vladislavkulakov <kulikbass@gmail.com>
2024-02-01 22:46:07 +0300

    Задание создание метода

---
+++ src/HomeWorkTests.java
@@ -0,0 +1,81 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+
+public class HomeWorkTests {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("appium:deviceName", "");
+        capabilities.setCapability("appium:platformVersion", "13.0");
+        capabilities.setCapability("appium:appPackage", "org.wikipedia");
+        capabilities.setCapability("appium:appActivity", ".main.MainActivity");
+        capabilities.setCapability("appium:automationName", "UiAutomator2");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), capabilities);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+    @Test
+    public void HomeWorkTest() {
+        waitElementAndClick(By.xpath("//android.widget.Button[@resource-id=\"org.wikipedia:id/fragment_onboarding_skip_button\"]"),
+                "Cannot find skip button",
+                15);
+
+        assertElementHasText(By.xpath("//android.widget.TextView[@text=\"Search Wikipedia\"]"),
+                "Search Wikipedia",
+                "Element hasn't text from variable'");
+
+    }
+
+
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage) {
+        return waitForElementPresent(by, errorMessage, 5);
+    }
+
+    private WebElement waitElementAndClick(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement assertElementHasText(By by, String value, String errorMessage) {
+        WebElement element = waitForElementPresent(by, errorMessage);
+        String elementText = element.getText();
+        Assert.assertEquals("Element hasn't text",
+                elementText,
+                value);
+        return element;
+
+    }
+
+}
